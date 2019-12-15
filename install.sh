6f6a4c23f27546cf2d31e89570c62a6610fd25a1
andrewbrannan <andrew.brannan@gmail.com>
2019-12-15 12:49:27 +0000

    add comments to install script

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+echo "### Starting dotfiles installation"
+
 # Get the directory this script is in
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Install oh-my-zsh if not already installed
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "### Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 1> /dev/null
 
 # Create symlinks of each dot file to home directory
+echo "### Linking from $HOME to dotfiles" 
 ln -sf $SCRIPT_DIR/.vimrc $HOME/.vimrc
 ln -sf $SCRIPT_DIR/.zshrc $HOME/.zshrc
 ln -sf $SCRIPT_DIR/.vimrc $HOME/.tmux.conf
+
+echo "### Done!"
